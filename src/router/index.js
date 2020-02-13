f9f1340a27a604fa1bf1a0c8304d8759de8aadbf
lychunvira18 <lychunvira18@kit.edu.kh>
2020-02-13 20:47:46 +0700

    restructured the structure of the project and added minor changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Login from "../views/loginForm";
-import Confirm from "../views/Confirmation";
+import Login from "../views/Login";
+import Confirm from "../views/LoginConfirm";
 import Home from "../views/Home"
 
 Vue.use(VueRouter);
@@ -9,17 +9,17 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    name: "home",
+    name: "Home",
     component: Home
   },
   {
     path: "/login",
-    name: "login",
+    name: "Login",
     component: Login
   },
   {
-    path: "/confirmation",
-    name: "confirmation",
+    path: "/login/LoginConfirm",
+    name: "LoginConfirm",
     component: Confirm
   }
 ];
